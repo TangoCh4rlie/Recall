00e93307ece3392977b1d13a12caea66d23289c1
TangoCharlie <elouanreymond@tutanota.com>
2024-09-03 20:09:19 +0200

    Initial Commit

---
+++ Recall/RecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallApp.swift
+//  Recall
+//
+//  Created by Elouan Reymond on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
